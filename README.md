1bdf5fe76f7c0652c9be6c474525ecf6b7410110
augusto-market <73326313+augusto-merc@users.noreply.github.com>
2025-03-11 13:09:22 +0000

    end-beginning
    Co-authored-by: AndreSilva1307 <AndreSilva1307@users.noreply.github.com>
    Co-authored-by: Abner Saconi <AbnerMSaconi@users.noreply.github.com>

--- README.md
+++ README.md
@@ -1,1 +1,55 @@
 # trabalho1-algoritmos-aplicado
+Este trabalho prático visa avaliar se cada grupo composto obrigatoriamente por até 3
+discentes desenvolvem códigos em C/C++ usando programação estruturada por meio
+de modularização. Para isso deve obrigatoriamente seguir as regras abaixo:
+
+REGRAS:
+
+1. Inserir comentário no início com o nome e RA dos componentes do grupo bem
+como o projeto desenvolvido (1 ou 2 + 3 [para todos]) ;
+2. Obrigatoriamente inserir comentário para:
+a. Variáveis globais
+b. Declaração dos módulos
+c. Função main (): Explicar o seu fluxo
+3. Projetar os módulos de modo que possam resolver o problema seguindo a
+especificação: main, modulo1, modulo2, etc. Além de um menu com as
+operações específicas em cada projeto. Não pode conter solução desenvolvida
+na MAIN();
+4. Entregar no prazo;
+5. O feedback do professor pode ser via AVA quando não há necessidade de
+entrevista ou nestes casos(entrevistas) apresentar o conhecimento.
+6. TODOS DEVEM DESENVOLVER O PROJETO 3. Cada grupo receberá uma
+especificação para diferenciar os trabalhos.
+
+PROJETOS
+
+1. Faça um programa que carregue uma matriz 4x4 com números inteiros e
+verifique se a mesma compõe um quadrado mágico. Um quadrado mágico é
+formado quando a soma dos elementos de cada linha é igual à soma dos
+elementos de cada coluna e igual à soma dos elementos da diagonal principal e
+igual, também à soma dos elementos da diagonal secundária.
+
+
+3. Faça um programa que receba um vetor de registros com dois campos, uma
+String e um valor numérico (inteiro ou ponto flutuante, será definido pelo
+docente). Este programa deve oferecer via MENU as opções abaixo:
+
+TRABALHO PRÁTICO 01
+MODULARIZAÇÃO COM MATRIZES
+Algoritmos & Programação Aplicada
+Prof Dr. Alexsandro M. Carneiro
+a. CADASTRAR os dados
+b. IMPRIMIR os dados
+c. BUSCAR um dado informando uma String ou valor numérico. Deve
+retornar a sua posição na matriz ou em caso de inexistente informar o
+valor -1
+d. REMOVER um dado ao passar sua posição
+e. ATUALIZAR um dado ao informar sua posição e novos valores (String e
+valor numérico).
+Cada grupo receberá uma especificação, por exemplo, cadastro de carros (Placa
+e ano). Assim teremos projetos específicos.
+OBS:
+• Projeto o procedimento e/ou função
+• Usem ponteiros para passar vetores ou matrizes com parâmetros.
+• Testes de forma unitária e depois integrada para o programa final.
+Explicação no quadro.
--- trabalho1.c
+++ trabalho1.c
@@ -1,6 +1,95 @@
+/*
+    Autores:
+    - RA: 198020 | Nome: Augusto Cezar da Silva Mercado
+    - RA: 197590 | Nome: André Gonçalves da Silva
+    - RA: 197459 | Nome: Abner Matheus Moraes Saconi
+*/
+
+// Bibliotecas
 #include <stdio.h>
 
-int main() {
-    
+// Variáveis Globais
+const int size = 4; 
+int matrix[size][size]; 
+
+// Módulos e Procedimentos
+void inputMatrix(); // Procedimento
+int magic_square(int matrix[size][size]); // Módulo
+
+int main() 
+{
+
+    int option = 1;
+
+    while (option != 0) // Loop do menu
+    { 
+        printf("Insira a sua opcao: \n");
+        printf("0 - Sair\n");
+        printf("1 - Projeto 1\n");
+        printf("3 - Projeto 3\n");
+        scanf("%d", &option);
+        
+        switch (option) // Menu
+        { 
+            case 1: // Abre o primeiro projeto
+                printf("Programa de verificacao de quadrados magicos\n");
+                inputMatrix();
+                printf("O quadrado %s\n", magic_square(matrix) ? "eh magico" : "nao eh magico");
+                break;
+            case 3: // Abre o terceiro projeto
+                magic_square(matrix);
+                break;
+            case 0: // Sai do programa
+                printf("Saindo...\n");
+                break;
+            default: 
+                printf("Opcao invalida\n");
+                break;
+        }
+    }
+
     return 0;
 }
+
+
+
+void inputMatrix() // Faz a leitura do input da matriz salvando os dados na variável global
+{
+    printf("Insira os elementos da matriz %dx%d \n\n", size, size);
+    for (int i = 0; i < size; i++) {
+        for (int j = 0; j < size; j++) {
+            printf("Elemento [%d][%d]: ", i, j);
+            scanf("%d", &matrix[i][j]);
+        }
+    }
+}
+
+int magic_square(int matrix[size][size]) // Função que verifica se uma matriz é um quadrado mágico.
+// Retorna 1 (true) se for um quadrado mágico, 0 (false) caso contrário.
+{
+    int sum_reference_row = 0, sum_reference_column = 0;
+    int sum_reference_d1 = 0, sum_reference_d2 = 0;
+
+    for(int i=0; i<size; i++)
+    {
+        for (int j=0; j<size; j++)
+        {
+            sum_reference_row += matrix[i][j];
+            sum_reference_column += matrix[j][i];
+            if (i == 0)
+            {
+                sum_reference_d1 += matrix[j][j];
+                sum_reference_d2 += matrix[j][size-j-1];
+            }
+        }
+        if (sum_reference_row != sum_reference_column || sum_reference_column != sum_reference_d1 || sum_reference_d1 != sum_reference_d2)
+        {
+            return 0;
+        }
+        sum_reference_row = 0, sum_reference_column = 0;
+    }
+
+    return 1;
+
+}
+
